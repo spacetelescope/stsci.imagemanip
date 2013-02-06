c473bb6884fbb0096d0334348dc23a70806facd6
sienkiew <sienkiew@stsci.edu>
2013-02-06 20:30:12 +0000

    get version from new install
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.imagemanip/trunk@22267 fe389314-cf27-0410-b35b-8c050e845b92

--- stsci/imagemanip/__init__.py
+++ stsci/imagemanip/__init__.py
@@ -2,14 +2,7 @@ from __future__ import division
 
 import interp2d
 
-try:
-    from .version import (__version__, __svn_revision__,
-                          __svn_full_info__, __setup_datetime__)
-except ImportError:
-    __version__ = ''
-    __svn_revision__ = ''
-    __svn_full_info__ = ''
-    __setup_datetime__ = None
+from .version import *
 
 import stsci.tools.tester
 def test(*args,**kwds):
