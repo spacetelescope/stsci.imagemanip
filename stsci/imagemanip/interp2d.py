7256de3d1973318be9a3f8265051ffeefc933764
bsimon <bsimon@stsci.edu>
2015-04-20 14:09:38 +0000

    Fixed import bug for bilinearinterp
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.imagemanip/trunk@39346 fe389314-cf27-0410-b35b-8c050e845b92

--- stsci/imagemanip/interp2d.py
+++ stsci/imagemanip/interp2d.py
@@ -1,7 +1,7 @@
 from __future__ import division
 
 import numpy as n
-from bilinearinterp import bilinearinterp as lininterp
+from . import bilinearinterp 
 
 def expand2d(image,outputsize):
     """
@@ -23,7 +23,7 @@ def expand2d(image,outputsize):
     """
     if (outputsize[0] >= image.shape[0] and outputsize[1] >= image.shape[1]):
         newimage = n.empty(outputsize,dtype=image.dtype)
-        lininterp(image,newimage)
+        bilinearinterp.bilinearinterp(image,newimage)
     else:
         raise ValueError("Output shape must be of larger dimension than input image.")
 
