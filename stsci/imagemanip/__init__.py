b982674bd4319adacd6e59a244359a386f8fffe4
Joseph Hunkeler <jhunkeler@gmail.com>
2018-08-10 16:03:09 -0400

    Purge test function

--- stsci/imagemanip/__init__.py
+++ stsci/imagemanip/__init__.py
@@ -1,9 +1,3 @@
 from __future__ import division
-
 from . import interp2d
-
 from .version import *
-
-import stsci.tools.tester
-def test(*args,**kwds):
-    stsci.tools.tester.test(modname=__name__, *args, **kwds)
